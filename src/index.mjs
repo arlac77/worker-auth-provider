7ab96df2b36b1d4f08326d551f8c393e69c622a1
Markus Felten <Markus.Felten@gmx.de>
2023-02-01 13:40:23 +0100

    fix: default object

--- src/index.mjs
+++ src/index.mjs
@@ -7,22 +7,24 @@ import { createCors } from "itty-cors";
 const router = Router();
 
 router
-//  .all("*", preflight) // handle CORS preflight/OPTIONS requests
+  //  .all("*", preflight) // handle CORS preflight/OPTIONS requests
   .get("/version", () => json({ version: "0.1.0" }))
   .post("/", authorize)
   .all("*", () => missing("Are you sure about that?"));
 
-export default function fetch(...args) {
-  return router
+export default {
+  fetch(...args) {
+    return router
       .handle(...args)
       .catch(err => error(500, err.stack))
       .then(corsify);
-}
+  }
+};
 
 /**
  * Respond to the request
  * @param {Request} request
- */ 
+ */
 async function authorize(request) {
   return new Response({ access_token: "token" }, { status: 200 });
 }
