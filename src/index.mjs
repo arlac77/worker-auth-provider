e399db337511c078921f6721a1072792614310f6
Markus Felten <Markus.Felten@gmx.de>
2023-02-04 23:40:17 +0100

    fix: enable cors

--- src/index.mjs
+++ src/index.mjs
@@ -3,13 +3,13 @@ import { error, json, missing } from "itty-router-extras";
 import { createCors } from "itty-cors";
 import { authorize } from "./authorize.mjs";
 
-//const { preflight, corsify } = createCors();
+const { preflight, corsify } = createCors({ allowOrigin: "*" });
 
 const router = Router();
 
 router
-  //  .all("*", preflight) // handle CORS preflight/OPTIONS requests
-  .get("/version", () => json({ version: "0.2.0" }))
+  .all("*", preflight) // handle CORS preflight/OPTIONS requests
+  .get("/version", () => json({ version: "0.3.0" }))
   .post("/", authorize)
   .all("*", () => missing("Are you sure about that?"));
 
@@ -18,6 +18,6 @@ export default {
     return router
       .handle(...args)
       .catch(err => error(500, err.stack))
-   //   .then(corsify);
+      .then(corsify);
   }
 };
