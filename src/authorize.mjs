8e972ce313cd7631b91a6a16fc18b76c24cddb02
Markus Felten <Markus.Felten@gmx.de>
2023-02-08 11:47:18 +0100

    fix: use @tsndr/cloudflare-worker-jwt

--- src/authorize.mjs
+++ src/authorize.mjs
@@ -1,19 +1,6 @@
-import sha256 from "crypto-js/sha256";
 import jwt from "@tsndr/cloudflare-worker-jwt";
 
 const secret = "sasffaFAFA35";
-const header = { alg: "HS256", typ: "JWT" };
-
-function encode(data) {
-  return btoa(JSON.stringify(data));
-}
-
-function sign(header, user, secret) {
-  const encodedHeader = encode(header);
-  const encodedPayload = encode(user);
-  const signature = btoa(encodedHeader + "." + encodedPayload + secret);
-  return encodedHeader + "." + encodedPayload + "." + signature;
-}
 
 /**
  * Respond to the request
@@ -21,7 +8,10 @@ function sign(header, user, secret) {
  */
 export async function authorize(request) {
   const scope = ["a", "b", "c"].join(",");
-  const access_token = sign(header, username, secret);
+
+  const access_token = jwt.sign({ username }, secret, {
+    expiresIn: "2h"
+  });
 
   return new Response(
     { scope, token_type: "bearer", access_token },
